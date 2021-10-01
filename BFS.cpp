69425aae06efc6a5db3e5eec0892a1ae95c3a14b
Malhar Patel <leofierus@gmail.com>
2021-10-01 23:36:12 +0530

    Updated the core language
    Added C++ code instead of the previous Python code which seemed a little bland and laggy

--- BFS.cpp
+++ BFS.cpp
@@ -1,66 +1,42 @@
-# Python3 Program to print BFS traversal
-# from a given source vertex. BFS(int s)
-# traverses vertices reachable from s.
-from collections import defaultdict
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vll;
 
-# This class represents a directed graph
-# using adjacency list representation
-class Graph:
+void addEdge(vll adj[], ll u, ll v){
+	adj[u].push_back(v);
+	adj[v].push_back(u);
+}
 
-	# Constructor
-	def __init__(self):
+void BFS(ll u, vll adj[], vector<bool> &visited){
+	queue<ll> q;
+	q.push(u);
+	visited[u]=true;
+	while(!q.empty()){
+		ll f=q.front();
+		cout<<f<<" ";
+		q.pop();
+		for(auto i=adj[f].begin(); i!=adj[f].end();i++){ //using auto to automatically determine type of i
+			if(visited[*i]==false){
+				q.push(*i);
+				visited[*i]=true;
+			}//*operator used because idk the type and have used i
+		}
+	}
+}
+int main(){
+	ll V=5;
+	vll adj[V];
+	vector<bool> visited(V, false);
+	addEdge(adj, 0, 1); 
+  	addEdge(adj, 0, 4); 
+  	addEdge(adj, 1, 2); 
+	addEdge(adj, 1, 3); 
+	addEdge(adj, 1, 4); 
+	addEdge(adj, 2, 3); 
+	addEdge(adj, 3, 4); 
 
-		# default dictionary to store graph
-		self.graph = defaultdict(list)
-
-	# function to add an edge to graph
-	def addEdge(self,u,v):
-		self.graph[u].append(v)
-
-	# Function to print a BFS of graph
-	def BFS(self, s):
-
-		# Mark all the vertices as not visited
-		visited = [False] * (max(self.graph) + 1)
-
-		# Create a queue for BFS
-		queue = []
-
-		# Mark the source node as
-		# visited and enqueue it
-		queue.append(s)
-		visited[s] = True
-
-		while queue:
-
-			# Dequeue a vertex from
-			# queue and print it
-			s = queue.pop(0)
-			print (s, end = " ")
-
-			# Get all adjacent vertices of the
-			# dequeued vertex s. If a adjacent
-			# has not been visited, then mark it
-			# visited and enqueue it
-			for i in self.graph[s]:
-				if visited[i] == False:
-					queue.append(i)
-					visited[i] = True
-
-# Driver code
-
-# Create a graph given in
-# the above diagram
-g = Graph()
-g.addEdge(0, 1)
-g.addEdge(0, 2)
-g.addEdge(1, 2)
-g.addEdge(2, 0)
-g.addEdge(2, 3)
-g.addEdge(3, 3)
-
-print ("Following is Breadth First Traversal"
-				" (starting from vertex 2)")
-g.BFS(2)
-
-
+	BFS(0,adj,visited);
+	cout<<endl;
+	return 0; 
+}
